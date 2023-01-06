3c59451264f533984832bdd2c0c55ba720132d49
jonjab <jonjab@ucsb.edu>
2023-01-06 14:54:58 -0800

    adding to gapminder exploration based on ep 4 and 5

--- exploring_gapminder.R
+++ exploring_gapminder.R
@@ -14,12 +14,30 @@ range(gapminder$lifeExp)
 #yikes, look at the lowest life exp. 23!
 
 lowest_life <- min(gapminder$lifeExp)
+lowest_life
 
 # how can I get the whole row?
-lowest_life_record <- gapminder[gapminder$lifeExp == lowest_life,]
-lowest_life_record
+gapminder[(gapminder$lifeExp == lowest_life) ,]
 
+# get all the Rwandas
 
-
+# not useful for everything.
 summary(gapminder)
 
+# episode 4 explorations
+# a: which columns are factors?
+# b: what are the values?
+str(gapminder)
+
+# a: only 1
+# b: there's 142 of them
+
+# ep 5 add on
+# add country GDP as a column with cbind
+
+countryGDP <- gapminder$pop * gapminder$gdpPercap
+# ground truth it. They seem reasonable
+range(countryGDP)
+
+gapminder <- cbind(gapminder, countryGDP)
+str(gapminder)
