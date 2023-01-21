8557a81f9027c1295ed9d087d05a0e27a7b79411
jonjab <jonjab@ucsb.edu>
2023-01-20 16:43:06 -0800

    notes for 5 and 6. 5 is done

--- ep5.r
+++ ep5.r
@@ -61,7 +61,9 @@ human_age
 
 # 1/7, 2/7, 3/7, 4/7, 4/7 
 
-# lets clean up cats
+# removing rows
+# let's clean up cats
+# by removing that row
 cats
 
 cats[-4,]
@@ -69,98 +71,78 @@ cats[-4,]
 # or all rows with NA's. Same result.
 na.omit(cats)
 
-# don't forget to re-assing
+# don't forget to re-assign.
 cats <- na.omit(cats)
 
-# we can remove coluns similarly
+# we can remove columns similarly
+cats
+str(cats)
 
+cats[,-4]
 
+# appending to dataframes
+# columns are vectors (all same type)
+# rows are lists (mixed types, which we skipped)
 
+# you can add 2 dataframes together
+cats <- rbind(cats, cats)
 
+# alt challenge: can you 
+# use cbind to add 2 dataframes together?
+cats <- cbind(cats, cats)
+# indeed you can.
+cats
+
+# skip challenge 2?
 
 
 ##########
 # GapMinder: under 'Realistic example'
+gapminder <- read.csv("data/gapminder_data.csv", stringsAsFactors = TRUE)
 
 # first thing to do is re-inforce factors.
+# country and continent are the factors here.
+str(gapminder)
+
+# don't worry about the alternatives for reading in files.
+
+# quick summary statistics on each column
+# for the factors, it shows a count of each
+# value
+summary(gapminder)
+
+str(gapminder$country)
+
+# weird eh?
+# factors are stored as integers
+typeof(gapminder$country)
+# but display as text.
+gapminder$country
+
+# measure the size of it
+nrow(gapminder)
+ncol(gapminder)
+dim(gapminder)
+colnames(gapminder)
 
 
+# Challenge 3
+# middle and end of your data
 
-####################
-# Where's this come from?
-x <- c(5.3, 6.2, 7.1, 4.8, 7.5)
-names(x) <- c(letters[1:5])
+# challenge 4
+# write a new script that downloads
+# gapminder from the internet
+# and creates a dataframe.
+# call it challenge4.r
+# and save it to the root of your project folder.
+# Clean your environment and run your script
 
-x
-
-# 3 ways to pull out values
-# like letters above
-x[1]
-x[4]
-
-# we can get multiples
-x[c(1, 3)]
-
-x[1:4]
-
-1:4
-
-# arbitrarily bring them out over and over
-x[c(1,3,4,2,2,1)]
-
-# place zero is empty
-# beyond the end is NA
-x[0]
-x[10]
-
-# exclude
-x[-2]
+# hint: the lesson script shows you how.
+# we skipped it.
 
 
-# challenge 1
-x <- c(5.4, 6.2, 7.1, 4.8, 7.5)
-names(x) <- c('a', 'b', 'c', 'd', 'e')
-print(x)
+# challenge 5 is good if we are going into a break.
 
-# get out b, c, and d
-x[c(b,c,d)]
-x[2:4]
-x[letters[2:4]]
-
-# because
-letters[2:4]
-
-# from the lesson:
-x[-c(1,5)]
-
-# lines 35-36: we took 2 steps
-# we can name on the fly
-
-# 10 minutes
-
-# subset with ==
-# and call by name
-x[names(x) == "a"]
-
-# & --logical and
-# | --logical or
-# both return a logical (True or False)
-
-# challenge 2
-# same vector if you haven't changed it:
-x <- c(5.4, 6.2, 7.1, 4.8, 7.5)
-names(x) <- c('a', 'b', 'c', 'd', 'e')
-print(x)
-
-x[x > 4 & x < 7]
-
-# more readable
-x[(x > 4) & (x < 7)]
-
-x_LT_example <- x[(x > 4) & (x < 7)]
-x_LT_example
-
-# I think challenge 3 goes here
 
 
 
---
+++ ep6.r
@@ -0,0 +1,78 @@
+# Episode 6
+# Subsetting Data
+
+me from?
+x <- c(5.3, 6.2, 7.1, 4.8, 7.5)
+names(x) <- c(letters[1:5])
+
+x
+
+# 3 ways to pull out values
+# like letters above
+x[1]
+x[4]
+
+# we can get multiples
+x[c(1, 3)]
+
+x[1:4]
+
+1:4
+
+# arbitrarily bring them out over and over
+x[c(1,3,4,2,2,1)]
+
+# place zero is empty
+# beyond the end is NA
+x[0]
+x[10]
+
+# exclude
+x[-2]
+
+
+# challenge 1
+x <- c(5.4, 6.2, 7.1, 4.8, 7.5)
+names(x) <- c('a', 'b', 'c', 'd', 'e')
+print(x)
+
+# get out b, c, and d
+x[c(b,c,d)]
+x[2:4]
+x[letters[2:4]]
+
+# because
+letters[2:4]
+
+# from the lesson:
+x[-c(1,5)]
+
+# lines 35-36: we took 2 steps
+# we can name on the fly
+
+# 10 minutes
+
+# subset with ==
+# and call by name
+x[names(x) == "a"]
+
+# & --logical and
+# | --logical or
+# both return a logical (True or False)
+
+# challenge 2
+# same vector if you haven't changed it:
+x <- c(5.4, 6.2, 7.1, 4.8, 7.5)
+names(x) <- c('a', 'b', 'c', 'd', 'e')
+print(x)
+
+x[x > 4 & x < 7]
+
+# more readable
+x[(x > 4) & (x < 7)]
+
+x_LT_example <- x[(x > 4) & (x < 7)]
+x_LT_example
+
+# I think challenge 3 goes here
+
