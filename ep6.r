bc0c7fa4cc2a2f9e02face7a828a8254f183bf2a
Kristi Liu <ksliu@ucsb.edu>
2023-01-25 12:31:28 -0800

    edits to episode 6

--- ep6.r
+++ ep6.r
@@ -27,19 +27,23 @@ x[c(1,3,4,2,2,1)]
 
 # place zero is empty
 # beyond the end is NA
-x[0]
-x[10]
+x[0] #not counting from 0
+x[10] #there is no 10th item
 
-# exclude
+# to exclude an item from an index 
 x[-2]
+# this will return everything but the 2nd element
 
+#or in multiples
+x[c(-1,-5)]
+x[-c(1,5)]
 
 # challenge 1
 x <- c(5.4, 6.2, 7.1, 4.8, 7.5)
 names(x) <- c('a', 'b', 'c', 'd', 'e')
 print(x)
 
-# get out b, c, and d
+# what are two wayts to get out b, c, and d?
 x[c(b,c,d)]
 x[2:4]
 x[letters[2:4]]
@@ -55,13 +59,20 @@ x[-c(1,5)]
 
 # 10 minutes
 
+#Subsetting by name 
+x <- c(a=5.4, b=6.2, c=7.1, d=4.8, e=7.5) 
+x[c("a", "c")]
+
+
 # subset with ==
 # and call by name
 x[names(x) == "a"]
 
 # & --logical and
 # | --logical or
+# Use > < == to evaluate logical vectors
 # both return a logical (True or False)
+# ! is the logical NOT operator 
 
 # challenge 2
 # same vector if you haven't changed it:
@@ -74,8 +85,53 @@ x[x > 4 & x < 7]
 # more readable
 x[(x > 4) & (x < 7)]
 
-x_LT_example <- x[(x > 4) & (x < 7)]
-x_LT_example
+x_example <- x[(x > 4) & (x < 7)]
+x_example
 
-# I think challenge 3 goes here
+#multiple elements can have the same name:
+x <- 1:3
+names(x) <- c('a','a','a')
+x
 
+#Skipping names elements
+x <- c(a=5.4, b=6.2, c=7.1, d=4.8, e=7.5)
+# you might try: 
+x[-"a"]
+
+# the correct way to use logicals in this situation is: 
+x[names(x) != "a"]
+
+#try dropping multiple elements 
+x[names(x) !=c("a","c")]
+
+
+names(x) !=c("a","c")
+
+
+
+
+#Using %in%
+# If you want to match each element of the left argument with all of the elements of the right argument
+
+
+
+# challenge 3 - skip if low on time
+# Use gapminder dataset
+# pull out information from gapminder to add to the seAsia vector we just made. 
+#pull out information from southeast Asia: how do we set up an operation to produce a logical vector that is TRUE for all of the countries in southeast Asia and FALSE otherwise?
+seAsia <- c("Myanmar","Thailand","Cambodia","Vietnam","Laos")
+seAsia
+## read in the gapminder data that we downloaded in episode 2
+gapminder <- read.csv("data/gapminder_data.csv", header=TRUE)
+## extract the `country` column from a data frame (we'll see this later);
+## convert from a factor to a character;
+## and get just the non-repeated elements
+countries <- unique(as.character(gapminder$country))
+countries
+
+#the clunky answer
+(countries=="Myanmar" | countries=="Thailand" |
+    countries=="Cambodia" | countries == "Vietnam" | countries=="Laos")
+
+#cleaner answer: 
+countries %in% seAsia
