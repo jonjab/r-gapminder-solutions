97fedd65ee92b89ae4663980bc7a61fbe2e78769
jonjab <jonjab@ucsb.edu>
2023-01-26 15:01:37 -0800

    review and ep 8 work.

--- ep8.r
+++ ep8.r
@@ -48,8 +48,10 @@ ggplot(data = gapminder,
        mapping = aes(x=year, y=lifeExp, by=continent, color=continent)) +
   geom_line()
 
+
 # 2 geoms is totally allowed!
-# both geoms get the color from continent
+# here, both geoms get their color from 
+# the global aes()
 ggplot(data = gapminder, 
        mapping = aes(x=year, y=lifeExp, by=country, color=continent)) +
   geom_line() + 
@@ -57,14 +59,37 @@ ggplot(data = gapminder,
 
 # the points really are on on top
 # this one stresses that
+ggplot(data = gapminder, 
+       mapping = aes(x=year, y=lifeExp, by=country, color=continent)) +
+  geom_line() + 
+  geom_point(color="black")
+
+
+# this doesn't work
+ggplot(data = gapminder, 
+       mapping = aes(x=year, y=lifeExp, by=country), color=continent) +
+  geom_line() + 
+  geom_point(mapping = aes(color="blue"))
+
+
 # moving color to the geom_ lets you assign them seperately
 ggplot(data = gapminder, 
        mapping = aes(x=year, y=lifeExp, by=country)) +
   geom_line(mapping = aes(color=continent)) + 
   geom_point(mapping = aes(color="blue"))
 
+# you can also have a local mapping and aes like this:
+ggplot(data = gapminder, 
+       mapping = aes(x=year, y=lifeExp, by=country)) +
+  geom_line(mapping=aes(color=continent)) + 
+  geom_point(color="blue")
 
-# do the tip here?
+# but you CAN'T do this:
+ggplot(data = gapminder, 
+       mapping = aes(x=year, y=lifeExp, by=country)) +
+  geom_line(color=continent) + 
+  geom_point(color="blue")
+
 
 # challenge 3
 # switch the order
@@ -73,19 +98,86 @@ ggplot(data = gapminder, mapping = aes(x=year, y=lifeExp, by=country)) +
   geom_line(mapping = aes(color=continent))
 
 
+
+# ########################
 # transformations and stats
 ggplot(data = gapminder, 
        mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point()
 
 # ^^ that looks like a log. let's try to straighten it
+# AND we will make those points transparent, since they are so stacked up
 ggplot(data = gapminder, 
        mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point(alpha = 0.5) + scale_x_log10()
 
-# add a line
+# add a trendline
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm")
 
+# pop quiz:
+# what are some other geom_smooth methods?
+
+# I also can control the confidence interval around the geom_smooth
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm", level=.75)
+
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm", level=.99)
+
+# let's pause and see what this graph is saying
+# an extra 10 years of life expectancy costs 10X as much gdp / person!
+
+# remember, this is multiple years
+# the trend is for all of these to go up
+ggplot(data = gapminder, mapping = aes(x = year, y = lifeExp)) +
+  geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm", level=.99)
+
+ggplot(data = gapminder, mapping = aes(x = year, y = gdpPercap)) +
+  geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm", level=.99)
+
+
+
+# and yes we can change the size of the trend line
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method="lm", size=1.5)
+
+
+# ##############################
+# Challenge 4
+
+# Modify the color and size of the points on the point layer in the previous example.
+
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(alpha = 0.5, size = 4) + scale_x_log10() + geom_smooth(method="lm", size=1.5)
+
+# NOTE: the units. If you make the graph bigger, the points max out.
+# that's because those are `points'
+# so it's connected to some arbitrarily sized piece of paper.
+
+
+# Challend 4b
+# Modify your solution to Challenge 4a 
+# so that the points are now a different shape and are 
+# colored by continent with new trendlines. 
+# 
+# Hint: The color argument can be used inside the aesthetic.
+
+# start with the shape
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+  scale_x_log10() + 
+  geom_point(alpha = 0.5, size = 4, shape = 18) + 
+  geom_smooth(method="lm", size=1.5)
+
+# color the continent
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp, color=continent)) +
+  scale_x_log10() + 
+  geom_point(alpha = 0.5, size = 4, shape = 18) + 
+  geom_smooth(method="lm", size=1.5)
+
+
+# Multi-panel figures
+
+# let's just do Americas
+americas <- gapminder[gapminder$continent == "Americas",]
+
