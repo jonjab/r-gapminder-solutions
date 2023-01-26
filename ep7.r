69850b0307328a81f9cbf19e2e1962dc6c01afae
Kristi Liu <ksliu@ucsb.edu>
2023-01-26 15:02:17 -0800

    moving into episode 7 god dplyr is a relief

--- ep7.r
+++ ep7.r
@@ -5,7 +5,7 @@
 gapminder <- read.csv("data/gapminder_data.csv", stringsAsFactors = TRUE)
 
 
-unique(gapminder$continent)
+# unique(gapminder$continent)
 mean(gapminder[gapminder$continent == "Africa", "gdpPercap"])
 mean(gapminder[gapminder$continent == "Americas", "gdpPercap"])
 
@@ -19,7 +19,7 @@ library(dplyr)
 # dataframe, column, column, ...
 year_country_gdp <- select(gapminder, year, country, gdpPercap)
 year_country_gdp
-dim(year_country_gdp)
+#dim(year_country_gdp)
 
 # if you want to eliminate 1 column, use minue on that column
 smaller_gapminder <- select(gapminder, -continent)
@@ -55,8 +55,7 @@ europe_lifeExp_2007
 
 # challenge 1
 # write ONE command to get a dataframe
-# with African
-# lifeExp, country, and year.
+# with African lifeExp, country, and year.
 # how many rows do you get out?
 
 challenge1 <- gapminder %>% 
@@ -75,5 +74,43 @@ dim(challenge1)
 
 
 # group by
-# so we can see series
+# reduces need for repetition and the errors that come with it
+# so we can see series, essentially use every unique criteria that could have 
+# been used in filter 
+
+str(gapminder)
+
+#with group_by()
+str(gapminder %>% group_by(continent))
+
+
+#using Summarize
+#summarize will create a new variable by using functions that repreat for each
+#continent specific datafram 
+
+gdp_bycontinent <- gapminder %>% 
+  group_by(continent) %>% 
+  summarize(mean_gdpPercap = mean(gdpPercap))
+
+gdp_bycontinent
+
+#Now you try!
+# Challenge 2
+# calculate the average life expectancy per country
+# which one has the highest? lowest? 
+
+lifeExp_bycountry<- gapminder %>% 
+  group_by(country) %>% 
+  summarize(mean_lifeExp = mean(lifeExp))
+
+lifeExp_bycountry %>% 
+  filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+
+# grouping by multiples 
+gdp_bycontinents_byyear <- gapminder %>%
+  group_by(continent, year) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap))
+
+
+# Using count() and n()
 
